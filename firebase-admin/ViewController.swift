70f3527a87cde5537fad545604af4db8e955e0cb
Chris Ellsworth <cellsworth@gmail.com>
2015-11-22 13:22:27 -0500

    Initial Commit

---
+++ firebase-admin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebase-admin
+//
+//  Created by Chris Ellsworth on 11/22/15.
+//  Copyright © 2015 Chris Ellsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
