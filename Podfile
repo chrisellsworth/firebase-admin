2bac64b814dfd4ad8dcdf5146e3c5a166f894d21
Chris Ellsworth <cellsworth@gmail.com>
2015-11-29 22:05:31 -0500

    Fork of FirebaseUI pod

--- Podfile
+++ Podfile
@@ -3,4 +3,5 @@ use_frameworks!
 
 target 'firebase-admin' do
     pod 'Firebase', '~> 2.4'
+    pod 'FirebaseUI/Core', :git => "https://github.com/chrisellsworth/FirebaseUI-iOS.git", :branch => 'raise-cancelations'
 end
--- Pods/Target Support Files/Pods-firebase-admin/Pods-firebase-admin-frameworks.sh
+++ Pods/Target Support Files/Pods-firebase-admin/Pods-firebase-admin-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-firebase-admin/Firebase.framework"
+  install_framework "Pods-firebase-admin/FirebaseUI.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-firebase-admin/Firebase.framework"
+  install_framework "Pods-firebase-admin/FirebaseUI.framework"
 fi
